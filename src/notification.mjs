685d52c01f06c128100d93f1fbfb798d6ed47b49
Rimu Eirnarn <Rimuru720@proton.me>
2025-01-14 17:48:38 +0700

    added id for notification control

--- src/notification.mjs
+++ src/notification.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import { config } from "./config.mjs";
 import { Template } from "./template.mjs";
+import { generate_id } from "./utils.mjs";
 
 const nl_template = new Template("null", "")
 
@@ -12,6 +13,17 @@ const nl_template = new Template("null", "")
  * @property {string?} title - notification title
  * @property {string?} level - notification level (INFO, WARNING, ERROR) 
  * @property {Number?} time - notification visible time
+ */
+
+/** Notification data history
+ * @typedef NotificationHistory
+ * @type {object}
+ * 
+ * @property {string} body - notification body
+ * @property {string?} title - notification title
+ * @property {string?} level - notification level (INFO, WARNING, ERROR) 
+ * @property {Number?} time - notification visible time
+ * @property {string} id notification ID
  */
 
 /**
@@ -31,65 +43,81 @@ const nl_template = new Template("null", "")
  */
 
 /**
+ * Initiator callback
+ * @callback initiator
+ * 
+ * @param {string} notification_id Notification ID
+ * @returns {string}
+ */
+
+/**
  * Notification
  */
 class Notification {
-    /** @type {NotificationData[]} */
-    #_history
-    /** @type {Template} */
-    #template
+  /** @type {NotificationHistory[]} */
+  #_history
+  /** @type {Template} */
+  #template
 
-    /** @type {timeFormatter} */
-    #timeFormatter
+  /** @type {timeFormatter} */
+  #timeFormatter
 
-    /** @type {levelFormatter} */
-    #levelFormatter
+  /** @type {levelFormatter} */
+  #levelFormatter
 
-    constructor() {
-        this.#_history = []
-        this.#template = nl_template
-        this.#timeFormatter = (time) => time.toString()
-        this.#levelFormatter = (level) => `bg-${level}`
+  /** @type {initiator} */
+
+  constructor() {
+    this.#_history = []
+    this.#template = nl_template
+    this.#timeFormatter = (time) => time.toString()
+    this.#levelFormatter = (level) => `bg-${level}`
+  }
+  /**
+  * Push Notification and renders it
+  * @method
+  * @param {NotificationData} data notification data
+  */
+  push(data) {
+    if (!this.#template.isEmpty()) {
+      const nid = generate_id(16)
+      /** @type {NotificationHistory} */
+      // @ts-ignore
+      const hist = data
+      hist.id = nid
+      if (config.target.notification == null)
+        throw new Error("Notification cannot be pushed: No mounting point")
+      this.#template.render(config.target.notification, {
+        title: data.title || "",
+        body: data.body,
+        time: this.#timeFormatter(data.time || 0) || "now",
+        level: this.#levelFormatter(data.level || 'info'),
+        id: nid
+      })
+      this.#_history.push(hist)
     }
-    /**
-    * Push Notification and renders it
-    * @method
-    * @param {NotificationData} data notification data
-    */
-    push(data) {
-        this.#_history.push(data)
-        if (!this.#template.isEmpty()) {
-            if (config.target.notification == null)
-                throw new Error("Notification cannot be pushed: No mounting point")
-            this.#template.render(config.target.notification, {
-                title: data.title || "",
-                body: data.body,
-                time: this.#timeFormatter(data.time || 0) || "now",
-                level: this.#levelFormatter(data.level || 'info')
-            })
-        }
-        console.log(`${data.title}\n${data.body}`)
-    }
+    console.log(`${data.title}\n${data.body}`)
+  }
 
-    /**
-     * Set template
-     * @param {Template} tmpl 
-     */
-    setTemplate(tmpl) {
-        this.#template = tmpl
-    }
+  /**
+   * Set template
+   * @param {Template} tmpl 
+   */
+  setTemplate(tmpl) {
+    this.#template = tmpl
+  }
 
-    /**
-     * Set time formatter template
-     * @param {timeFormatter} fmt 
-     */
-    setTimeFormatter(fmt) {
-        this.#timeFormatter = fmt
-    }
+  /**
+   * Set time formatter template
+   * @param {timeFormatter} fmt 
+   */
+  setTimeFormatter(fmt) {
+    this.#timeFormatter = fmt
+  }
 
-    get history() {
-        return this.#_history
-    }
+  get history() {
+    return this.#_history
+  }
 }
 
 export { Notification }
